ffe3683b1f3aec00e52b7c30eeeae9923083cfca
acmo0 <acmo0@hackademint.org>
2024-05-14 22:27:05 +0200

    Fix BS + API feature server side

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 import base64
 
 AI_CONTAINER_IP = "172.17.0.2"
+
 if 'AI_CONTAINER_IP' in os.environ:
     AI_CONTAINER_IP = os.environ['AI_CONTAINER_IP'] # retrieve IP of the docker which runs the AI and API
 print("AI IP at",AI_CONTAINER_IP)
@@ -27,28 +28,36 @@ def explanations():
     return render_template("about.html")
 
 
-# -- API --
+# -- API --
 @app.route('/generate',methods = ['GET', 'POST'])
 def generate():
     """
     Backend function to call API when user ask to generate an image
     """
+
     api_result = None
+
     if request.method == 'GET' or request.data == "": # Only generation without prompt
         r = rq.get("http://"+AI_CONTAINER_IP+":5000/api_v1/generate") # send request to AI/API container
         api_result = json.loads(r.text) # loads returned json
     else:
-
+        
         data = {'description':request.form.get('description')} # get data passed in post request
-        if 'description' in data and 'model' in data: # get prompt of user passed in post data
-            if data['model'] == "v1": # if user request to user the first model
-                r = rq.post("http://"+AI_CONTAINER_IP+":5000/api_v1/generate_prompt",data=json.dumps({'description': data['description']})) # send post request with user prompt as json data
-                api_result = json.loads(r.text)
 
-        elif 'description' in data: # if no model specified, redirect to first version of model
-                r = rq.post("http://"+AI_CONTAINER_IP+":5000/api_v1/generate_prompt",data=json.dumps({'description': data['description']})) # send post request with user prompt as json data
-                api_result = json.loads(r.text)
+        model_version_is_one = (request.form.get('is-version-one') is not None) and (request.form.get('is-version-one') == "on") # get selected model version
+
+        data['model'] = "v1" if model_version_is_one else "v2"
+
+        api_url = "http://" + AI_CONTAINER_IP + ":5000/api_" + data['model'] + "/generate_prompt" # construct the url to request the selected model
+        
+        try:
+            r = rq.post(api_url, data=json.dumps({'description': data['description']})) # send post request with user prompt as json data
+        except:
+            return render_template('index.html',b64imagetag="",visibility="d-none", api_error=True)
+
+        api_result = json.loads(r.text)
 
     if api_result is not None and api_result['status']:
         return render_template('index.html',b64imagetag=api_result['image'],visibility="") # render base64 encoded image
-    return render_template('base.html',visibility="d-none") # if something fails, retur base template
+
+    return render_template('base.html',visibility="d-none", api_error=True) # if something fails, retur base template
